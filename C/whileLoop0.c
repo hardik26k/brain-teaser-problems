e2569c33d24182b8e9e47f8c11ce22a5d55f99e6
hardik26k <hardik26k@gmail.com>
2022-08-18 01:06:18 +0530

    Added questions in C language

---
+++ C/whileLoop0.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+int i=4,j=7;
+while(++i<--j){
+printf("loop");
+}
+printf("%d %d",i,j);
+}
---
+++ C/whileLoop2.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+void main(){
+int i = 0;
+while(i++){
+printf("Loop");
+if(i==3)break;
+}
+}
